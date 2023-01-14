7dc689a8e9db8c01b4b679006d60c4774916d718
Jannik Taubert <uyxxq@student.kit.edu>
2023-01-14 22:15:39 +0200

    initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net;
+using System.Net.Quic;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Client;
+
+public static class Program
+{
+    public static async Task Main()
+    {
+        Console.WriteLine(Environment.OSVersion);
+        
+        var cert = X509Certificate.CreateFromCertFile("domain.crt");
+        var options = new QuicClientConnectionOptions()
+        {
+            DefaultCloseErrorCode = 1,
+            DefaultStreamErrorCode = 2,
+            IdleTimeout = TimeSpan.FromMinutes(1),
+            MaxInboundBidirectionalStreams = 0,
+            MaxInboundUnidirectionalStreams = 0,
+            ClientAuthenticationOptions = new SslClientAuthenticationOptions()
+            {
+                ApplicationProtocols = new List<SslApplicationProtocol>() { { SslApplicationProtocol.Http3 }},
+                ClientCertificates = new X509CertificateCollection() {{cert}},
+                RemoteCertificateValidationCallback = ((sender, certificate, chain, errors) => true),
+                LocalCertificateSelectionCallback = ((sender, host, certificates, certificate, issuers) => cert),
+                EnabledSslProtocols = SslProtocols.Tls,
+                CertificateRevocationCheckMode = X509RevocationMode.NoCheck,
+                TargetHost = "127.0.0.1",
+                EncryptionPolicy = EncryptionPolicy.RequireEncryption,
+                AllowRenegotiation = true
+            },
+            RemoteEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5432),
+        };
+        var connection = await QuicConnection.ConnectAsync(options);
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net;
+using System.Net.Quic;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Server;
+
+public static class Program
+{
+    public static async Task Main()
+    {
+        var cert = X509Certificate.CreateFromCertFile("domain.crt");
+        var options = new QuicListenerOptions()
+        {
+            ListenEndPoint = new IPEndPoint(IPAddress.Any, 5432),
+            ApplicationProtocols = new List<SslApplicationProtocol>() {{SslApplicationProtocol.Http3}},
+            ConnectionOptionsCallback = (async (quicConnection, info, arg3) =>
+            {
+                var options = new QuicServerConnectionOptions()
+                {
+                    DefaultCloseErrorCode = 1,
+                    DefaultStreamErrorCode = 2,
+                    IdleTimeout = TimeSpan.FromMinutes(1),
+                    MaxInboundBidirectionalStreams = 1,
+                    MaxInboundUnidirectionalStreams = 0,
+                    ServerAuthenticationOptions = new SslServerAuthenticationOptions()
+                    {
+                        ApplicationProtocols = new List<SslApplicationProtocol>() { { SslApplicationProtocol.Http3 } },
+                        ClientCertificateRequired = true,
+                        RemoteCertificateValidationCallback = ((sender, certificate, chain, errors) => true),
+                        EnabledSslProtocols = SslProtocols.Tls,
+                        ServerCertificate = cert,
+                        ServerCertificateSelectionCallback = ((sender, name) => cert),
+                        EncryptionPolicy = EncryptionPolicy.RequireEncryption,
+                        AllowRenegotiation = true
+                    }
+                };
+                return options;
+            })
+        };
+        var listener = await QuicListener.ListenAsync(options);
+        var connection = await listener.AcceptConnectionAsync();
+        Console.WriteLine("new connection");
+    }
+}
